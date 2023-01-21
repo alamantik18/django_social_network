f79d1f33ac29a13941ed4bcdbf58ff172e87775a
alamantik18 <59261897+alamantik18@users.noreply.github.com>
2023-01-22 01:00:19 +0200

    Delete connect_db.py

--- alamnet/connect_db.py
+++
@@ -1,8 +0,0 @@
-CONFIG_DB = {
-    'POSTGRES_ENGINE': 'django.db.backends.postgresql',
-    'POSTGRES_DATABASE': 'alamnet',
-    'POSTGRES_USER': 'postgres',
-    'POSTGRES_PASSWORD': '18R07d2002@',
-    'POSTGRES_HOST': 'localhost',
-    'POSTGRES_PORT': '5432',
-}
